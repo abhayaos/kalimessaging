b0e114dd238cf559a34a5af41498923d3736b924
nepalcodinghouse <nepalcodinghouse@gmail.com>
2026-02-13 17:26:33 +0545

    add mobile navbar

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -1,19 +1,30 @@
 // src/utils/Layout.jsx
-import React, { useState } from 'react'
+import React from 'react'
 import SideBar from '../components/SideBar'
+import MobileNavbar from '../Mobile/Navbar'
 
 const Layout = ({ children }) => {
-  const [hideSidebar, setHideSidebar] = useState(false)
-
   return (
     <div className="flex min-h-screen">
-      {/* Sidebar */}
-      {!hideSidebar && <SideBar />}
+
+      {/* Sidebar - Hidden on mobile, visible on md and up */}
+      <div className="hidden md:block">
+        <SideBar />
+      </div>
 
       {/* Main content */}
-      <main className={`flex-1 p-4 md:p-6`}>
-        {children}
-      </main>
+      <div className="flex-1 flex flex-col">
+
+        {/* Mobile Navbar - Visible only on mobile */}
+        <div className="block md:hidden">
+          <MobileNavbar />
+        </div>
+
+        <main className="flex-1 p-4 md:p-6">
+          {children}
+        </main>
+
+      </div>
     </div>
   )
 }
